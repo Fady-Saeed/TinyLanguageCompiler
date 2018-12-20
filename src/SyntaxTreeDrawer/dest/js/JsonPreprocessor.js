59d16bf89539343d4a9801237633ce819575817a
Fady Saeed <fady.saeed@outlook.com>
2018-12-21 00:44:49 +0200

    Recurse if the value of factor node is an object

--- src/SyntaxTreeDrawer/dest/js/JsonPreprocessor.js
+++ src/SyntaxTreeDrawer/dest/js/JsonPreprocessor.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -154,12 +156,16 @@ var JsonPreprocessor = function () {
                     };
                     break;
                 case types.factor:
-                    node = {
-                        id: currentCounter++,
-                        name: (isNaN(Number(currentNode.value)) ? "" + factorTypes.id : "" + factorTypes.const) + ("(" + currentNode.value + ")"),
-                        no_parent: !hasParent,
-                        container: containers.ellipse
-                    };
+                    if (_typeof(currentNode.value) === "object") {
+                        node = this.generateNode(currentNode.value);
+                    } else {
+                        node = {
+                            id: currentCounter++,
+                            name: (isNaN(Number(currentNode.value)) ? "" + factorTypes.id : "" + factorTypes.const) + ("(" + currentNode.value + ")"),
+                            no_parent: !hasParent,
+                            container: containers.ellipse
+                        };
+                    }
                     break;
                 case types.stmt_sequence:
                     var children = [];
