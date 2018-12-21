6a448375beb9be5661036f1ac561416ca87cc59d
Fady Saeed <FadySN@outlook.com>
2018-12-21 02:11:37 +0200

    Fix(#4)/Fix webview js reading from file
    Execute javascript code directly from the webview instead of saving to a file and reading from it

--- src/Main.java
+++ src/Main.java
@@ -19,25 +19,35 @@ import java.io.FileWriter;
 public class Main extends Application {
 
     public static Stage sharedPrimaryStage;
-
+    public static String json;
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        src.Compiler.Parser parser = new Parser("{ Sample program in TINY language – computes factorial\n" +
-                "}\n" +
-                "read x; {input an integer }\n" +
-                "if 0 < x then { don’t compute if x <= 0 }\n" +
-                "fact := 1;\n" +
+        src.Compiler.Parser parser = new Parser("x:=2;\n" +
+                "y:=3;\n" +
+                "z:=5;\n" +
+                "a:=x+y+z;\n" +
+                "if z<8 then\n" +
                 "repeat\n" +
-                "fact := fact * x;\n" +
-                "x := x - 1\n" +
-                "until x = 0;\n" +
-                "write fact { output factorial of x }\n" +
+                "a:=a*2;\n" +
+                "z:=z-1\n" +
+                "until\n" +
+                "z=0;\n" +
+                "write a\n" +
+                "else\n" +
+                "read b;\n" +
+                "if b=1 then \n" +
+                "write b*(x-y)\n" +
+                "else\n" +
+                "write a\n" +
+                "end \n" +
+                "\n" +
                 "end;\n" +
-                "read x");
+                "write z");
 
         Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
         String jsonOutput = gson.toJson(parser.program());
+        json = jsonOutput;
 
         BufferedWriter outputBR = new BufferedWriter(new FileWriter("./src/SyntaxTreeDrawer/dest/js/SyntaxTree.js"));
         outputBR.write("let json = " + jsonOutput);
